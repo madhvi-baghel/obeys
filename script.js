d7529e6be7a059bf561a70a61295bca0439c87b1
madhvi-baghel <madhvibaghel2000.com>
2024-03-23 21:46:56 +0530

    complete

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 function locomotiveScroll(){
     gsap.registerPlugin(ScrollTrigger);
 
@@ -29,6 +31,7 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 ScrollTrigger.refresh();
 
 }
+
 function loadingAnimation(){
     var tl = gsap.timeline()
     tl.from(".line h1",{
@@ -82,6 +85,7 @@ function loadingAnimation(){
         opacity:0
     },"-=1.2")
 }
+
 function cursorAnimation(){
     // document.addEventListener("mousemove",function (dets) {
     //     gsap.to("#crsr",{
@@ -89,14 +93,19 @@ function cursorAnimation(){
     //         top:dets.y
     //     })
     // })
-    Shery.mouseFollower({
-        //Parameters are optional.
-        skew: true,
-        ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-        duration: 1,
-      });
-    Shery.makeMagnet("#nav-part2 h4");
+   
 
+    if (!isMobileDevice) {
+        Shery.mouseFollower({
+          skew: true,
+          ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+          duration: 1,
+        });
+      }
+    
+      if (!isMobileDevice) {
+        Shery.makeMagnet("#nav-part2 h4");
+      }
 
     var videoContainer = document.querySelector("#video-container")
     var video = document.querySelector("#video-container video")
@@ -151,11 +160,41 @@ function cursorAnimation(){
 }
 function sheryAnimation(){
     // Shery.makeMagnet(".navItem");
-    Shery.imageEffect(".image-div",{
-        style: 2,
-        config:{"resolutionXY":{"value":100},"distortion":{"value":true},"mode":{"value":-3},"mousemove":{"value":0},"modeA":{"value":1},"modeN":{"value":0},"speed":{"value":1,"range":[-500,500],"rangep":[-10,10]},"frequency":{"value":50,"range":[-800,800],"rangep":[-50,50]},"angle":{"value":0.5,"range":[0,3.141592653589793]},"waveFactor":{"value":1.4,"range":[-3,3]},"color":{"value":10212607},"pixelStrength":{"value":3,"range":[-20,100],"rangep":[-20,20]},"quality":{"value":5,"range":[0,10]},"contrast":{"value":1,"range":[-25,25]},"brightness":{"value":1,"range":[-1,25]},"colorExposer":{"value":0.18,"range":[-5,5]},"strength":{"value":0.2,"range":[-40,40],"rangep":[-5,5]},"exposer":{"value":8,"range":[-100,100]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7333282495138598},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.7,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.37,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.31,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
-        gooey:true,
-    })
+    // Shery.imageEffect(".image-div",{
+    //     style: 2,
+    //     config:{"resolutionXY":{"value":100},"distortion":{"value":true},"mode":{"value":-3},"mousemove":{"value":0},"modeA":{"value":1},"modeN":{"value":0},"speed":{"value":1,"range":[-500,500],"rangep":[-10,10]},"frequency":{"value":50,"range":[-800,800],"rangep":[-50,50]},"angle":{"value":0.5,"range":[0,3.141592653589793]},"waveFactor":{"value":1.4,"range":[-3,3]},"color":{"value":10212607},"pixelStrength":{"value":3,"range":[-20,100],"rangep":[-20,20]},"quality":{"value":5,"range":[0,10]},"contrast":{"value":1,"range":[-25,25]},"brightness":{"value":1,"range":[-1,25]},"colorExposer":{"value":0.18,"range":[-5,5]},"strength":{"value":0.2,"range":[-40,40],"rangep":[-5,5]},"exposer":{"value":8,"range":[-100,100]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7333282495138598},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.7,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.37,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.31,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}},
+    //     gooey:true,
+    // })
+    if (!isMobileDevice) {
+        Shery.imageEffect(".image-div", {
+          style: 5,
+          gooey: true,
+          config: {
+            a: { value: 2, range: [0, 30] },
+            b: { value: 0.75, range: [-1, 1] },
+            zindex: { value: -9996999, range: [-9999999, 9999999] },
+            aspect: { value: 0.7241195453907675 },
+            gooey: { value: true },
+            infiniteGooey: { value: false },
+            growSize: { value: 4, range: [1, 15] },
+            durationOut: { value: 1, range: [0.1, 5] },
+            durationIn: { value: 1.5, range: [0.1, 5] },
+            displaceAmount: { value: 0.5 },
+            masker: { value: true },
+            maskVal: { value: 1.23, range: [1, 5] },
+            scrollType: { value: 0 },
+            geoVertex: { range: [1, 64], value: 1 },
+            noEffectGooey: { value: true },
+            onMouse: { value: 0 },
+            noise_speed: { value: 0.5, range: [0, 10] },
+            metaball: { value: 0.33, range: [0, 2] },
+            discard_threshold: { value: 0.5, range: [0, 1] },
+            antialias_threshold: { value: 0.01, range: [0, 0.1] },
+            noise_height: { value: 0.5, range: [0, 2] },
+            noise_scale: { value: 10, range: [0, 100] }
+          }
+        });
+      }
 }
 function flagAnimation(){
     document.addEventListener("mousemove",function(dets){
